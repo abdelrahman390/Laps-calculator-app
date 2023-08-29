86decdbc3a83d2eb8f0c35ca2779b2936543924f
abdelrahman390 <88147091+abdelrahman390@users.noreply.github.com>
2023-08-29 17:16:27 +0300

    Update main.js

--- main.js
+++ main.js
@@ -26,34 +26,51 @@ inputs[1].oninput = () => {
 }
 
 function finalResult() {
-    if (inputs[0].value !== "" && inputs[1].value !== "") {
-        var seconds,
-            minutes;
+    let seconds,
+        minutes,
+        calc;
 
-        if (timePerLap % 1 !== 0) {
-            seconds = +(+(timePerLap.split(".")[1]) * lapsNum);
-            if (lapsNum % 1 !== 0) {
-
-            }
-            minutes = (Math.trunc(timePerLap) * lapsNum) + Math.trunc((seconds / 60));
-            seconds = seconds % 60
-        } else {
-            minutes = (Math.trunc(timePerLap) * lapsNum)
-        }
-
-        if (seconds === undefined) {
-            finalTimeSpan.innerHTML = `${minutes} min`
-        } else {
-            if (lapsNum % 1 !== 0) {
-                if ((+Math.trunc(seconds) + (+lapsNum.split(".")[1] - 2) * 10) >= 60) {
-                    minutes += Math.trunc((+Math.trunc(seconds) + (+lapsNum.split(".")[1] - 2) * 10) / 60)
-                    seconds = Math.trunc((+Math.trunc(seconds) + (+lapsNum.split(".")[1] - 2) * 10) % 60)
-                    finalTimeSpan.innerHTML = `${Math.trunc(minutes)}.${+Math.trunc(seconds)} min`
-                }
-            } else {
-                finalTimeSpan.innerHTML = `${minutes}.${seconds} min`
-            }
-        }
-
+    if (timePerLap % 1 !== 0 && inputs[1].value.length !== 0) {
+        calc = (+timePerLap.split(".")[1] + 60) * (lapsNum)
+        minutes = Math.trunc(calc / 60)
+        seconds = calc % 60
+        finalTimeSpan.innerHTML = `${minutes}.${seconds}/Min`
+    } else if (inputs[1].value.length !== 0) {
+        calc = 60 * (lapsNum)
+        minutes = Math.trunc(calc / 60)
+        seconds = calc % 60
+        finalTimeSpan.innerHTML = `${minutes}.${seconds}/Min`
     }
+
 }
+
+    // if (inputs[0].value !== "" && inputs[1].value !== "") {
+    //     var seconds,
+    //         minutes;
+
+    //     if (timePerLap % 1 !== 0) {
+    //         seconds = +(+(timePerLap.split(".")[1]) * lapsNum);
+    //         // if (lapsNum % 1 !== 0) {
+
+    //         // }
+    //         minutes = (Math.trunc(timePerLap) * lapsNum) + Math.trunc((seconds / 60));
+    //         seconds = seconds % 60
+    //     } else {
+    //         minutes = (Math.trunc(timePerLap) * lapsNum)
+    //     }
+
+    //     if (seconds === undefined) {
+    //         finalTimeSpan.innerHTML = `${minutes} min`
+    //     } else {
+    //         if (lapsNum % 1 !== 0) {
+    //             if ((+Math.trunc(seconds) + (+lapsNum.split(".")[1] - 2) * 10) >= 60) {
+    //                 minutes += Math.trunc((+Math.trunc(seconds) + (+lapsNum.split(".")[1] - 2) * 10) / 60)
+    //                 seconds = Math.trunc((+Math.trunc(seconds) + (+lapsNum.split(".")[1] - 2) * 10) % 60)
+    //                 finalTimeSpan.innerHTML = `${Math.trunc(minutes)}.${+Math.trunc(seconds)} min`
+    //             }
+    //         } else {
+    //             finalTimeSpan.innerHTML = `${minutes}.${seconds} min`
+    //         }
+    //     }
+
+    // }
